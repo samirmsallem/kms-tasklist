ecc78166799cedfb299fa2a9efe4a420b4687574
Paul David Hiller <paul.david.hiller@mni.thm.de>
2022-05-03 16:37:03 +0200

    Categories in add modal

--- Frontend/src/app/add-task-modal/add-task-modal.component.ts
+++ Frontend/src/app/add-task-modal/add-task-modal.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {DataService} from "src/app/data.service";
 import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
+import {Category} from "src/app/task";
 
 @Component({
   selector: 'app-add-task-modal',
@@ -8,14 +9,21 @@ import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
   styleUrls: ['./add-task-modal.component.css']
 })
 export class AddTaskModalComponent implements OnInit {
-
   title: string;
+  CategoryList = Category;
+  Category: Category;
+
 
   constructor(public dataService: DataService, public activeModal: NgbActiveModal) {
     this.title = '';
+    this.Category = Category.abc;
   }
 
   ngOnInit(): void {
+  }
+
+  setCategory(cat: any){
+    this.Category = Object.keys(Category).indexOf(cat);
   }
 
   save() {
