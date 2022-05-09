9ca8fa559fec5eeb3aecfe857fd7d4490722b58c
Artem Grishin <artem.grishin@mni.thm.de>
2022-05-09 14:18:15 +0100

    import fix and tests for list.component.ts

--- Frontend/src/app/list/list.component.spec.ts
+++ Frontend/src/app/list/list.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ListComponent } from './list.component';
+
+import {HttpClientModule} from '@angular/common/http';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+//import {isArrayLike} from "rxjs/dist/types/internal/util/isArrayLike";
+import notEmpty = jasmine.notEmpty;
+import {Aufgabe} from "../aufgabe";
 
 describe('ListComponent', () => {
   let component: ListComponent;
@@ -8,6 +13,7 @@ describe('ListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       declarations: [ ListComponent ]
     })
     .compileComponents();
@@ -21,5 +27,51 @@ describe('ListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  // init start
+  it('should have an empty aufgabeList', () => {
+    expect(component.aufgabeList).not.toBeTruthy()
+    expect(component.listOfPriorities).toBeTruthy()
+  });
+
+  // should add task to list
+  it('should add Aufgabe', () => {
+
+    component.newAufgabe(new Aufgabe("abc",false, new Date()))
+    expect(component.aufgabeList?.includes( new Aufgabe("abc",false, new Date(1)))).toBeTruthy()
+  });
+
+  // if name of task not defined, task not in list
+  it('should not get Aufgabe',  () => {
+    component.newAufgabe(new Aufgabe("",false,new Date()))
+    expect(component.aufgabeList).not.toBeTruthy()
+  });
+
+  // if no tasks nothing to delete
+  it('should not get tasks',  () => {
+    expect(component.aufgabeList).not.toBeTruthy()
+    expect(component.deleteAufgabe(5)).not.toBeTruthy()
+
+  });
+
+  // todo delete tasks if they exists async?
+  it('should delete Aufgabe',  () => {
+    component.newAufgabe(new Aufgabe("abc",false, new Date(1)))
+    expect(component.aufgabeList).toContain(new Aufgabe("abc",false, new Date(1)))
+    component.deleteAufgabe(0)
+    expect(component.aufgabeList).toHaveSize(0)
+
+    component.newAufgabe(new Aufgabe("abc",false, new Date(1)))
+
+  });
+
+  // todo get list of tasks if they added async?
+  it('should get AufgabeList',  () => {
+    component.newAufgabe(new Aufgabe("abc",false, new Date()))
+    component.newAufgabe(new Aufgabe("abcd",false, new Date()))
+
+    expect(component.getAufgabeList()).toBeTruthy()
+
   });
 });
