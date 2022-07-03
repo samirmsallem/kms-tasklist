07f0f756d756ef401868c282bdbf5aea33db42bd
Samir Faycal Tahar M'Sallem <samir.faycal.tahar.m.sallem@mni.thm.de>
2022-07-03 22:13:42 +0200

    fix test cases

--- Frontend/src/app/aufgabe.service.spec.ts
+++ Frontend/src/app/aufgabe.service.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AufgabeService } from './aufgabe.service';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 
 describe('AufgabeService', () => {
   let service: AufgabeService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [HttpClientTestingModule],
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(AufgabeService);
   });
 
